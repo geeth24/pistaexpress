6f70fb036f7fddc19b40691a590eca55b289bc1f
Geeth S Gunnampalli <geeth.gunnampalli@gmail.com>
2022-12-07 12:24:25 -0600

    Update Contact Us Page

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,8 +1,151 @@
-import { Container, Text, Title } from "@mantine/core"
+import {
+    createStyles,
+    ThemeIcon,
+    Text,
+    Box,
+    Stack,
+    Container,
+    Title,
+} from "@mantine/core"
+import { IconPhone, IconMapPin, IconAt } from "@tabler/icons"
 import Head from "next/head"
-import React from "react"
+type ContactIconVariant = "white" | "gradient"
 
-function Contact() {
+interface ContactIconStyles {
+    variant: ContactIconVariant
+}
+
+const useStyles = createStyles((theme, { variant }: ContactIconStyles) => ({
+    wrapper: {
+        display: "flex",
+        alignItems: "center",
+        color: theme.white,
+    },
+
+    icon: {
+        marginRight: theme.spacing.md,
+        backgroundImage:
+            variant === "gradient"
+                ? `linear-gradient(135deg, ${
+                      theme.colorScheme === "dark"
+                          ? theme.colors[theme.primaryColor][7]
+                          : theme.colors[theme.primaryColor][4]
+                  } 0%, ${theme.colors[theme.primaryColor][6]} 100%)`
+                : "none",
+        backgroundColor: "transparent",
+    },
+
+    title: {
+        color:
+            variant === "gradient"
+                ? theme.colors.gray[6]
+                : theme.colors[theme.primaryColor][0],
+    },
+
+    description: {
+        color: variant === "gradient" ? theme.black : theme.white,
+    },
+}))
+
+interface ContactIconProps
+    extends Omit<React.ComponentPropsWithoutRef<"div">, "title"> {
+    icon: React.FC<any>
+    title: React.ReactNode
+    description: React.ReactNode
+    hrefPrefix: string
+    variant?: ContactIconVariant
+}
+
+function ContactIcon({
+    icon: Icon,
+    title,
+    description,
+    hrefPrefix,
+    variant = "gradient",
+    className,
+    ...others
+}: ContactIconProps) {
+    const { classes, cx } = useStyles({ variant })
+    return (
+        <div
+            className={cx(classes.wrapper, className)}
+            {...others}
+            onClick={() => window.open(hrefPrefix + description)}
+        >
+            {variant === "gradient" ? (
+                <ThemeIcon size={40} radius="md" className={classes.icon}>
+                    <Icon size={24} />
+                </ThemeIcon>
+            ) : (
+                <Box mr="md">
+                    <Icon size={24} />
+                </Box>
+            )}
+
+            <div>
+                <Text
+                    size="xs"
+                    className={classes.title}
+                    sx={(theme) => ({
+                        color:
+                            theme.colorScheme === "dark"
+                                ? theme.colors.lime[1]
+                                : theme.colors.lime[9],
+                    })}
+                >
+                    {title}
+                </Text>
+                <Text
+                    className={classes.description}
+                    sx={(theme) => ({
+                        color:
+                            theme.colorScheme === "dark"
+                                ? theme.white
+                                : theme.colors.lime[9],
+                    })}
+                >
+                    {description}
+                </Text>
+            </div>
+        </div>
+    )
+}
+
+interface ContactIconsListProps {
+    data?: ContactIconProps[]
+    variant?: ContactIconVariant
+}
+
+const MOCKDATA = [
+    {
+        title: "Email",
+        description: "pistamidland@gmail.com",
+        icon: IconAt,
+        hrefPrefix: "mailto:",
+    },
+    {
+        title: "Phone",
+        description: "+1 (432) 897-4533",
+        icon: IconPhone,
+        hrefPrefix: "tel:",
+    },
+    {
+        title: "Address",
+        description: "3312 Fortress Dr, Midland, TX 79706",
+        icon: IconMapPin,
+        hrefPrefix:
+            "https://www.google.com/maps/search/?api=1&query=Pista+Express+Midland",
+    },
+]
+
+function ContactIconsList({ data = MOCKDATA, variant }: ContactIconsListProps) {
+    const items = data.map((item, index) => (
+        <ContactIcon key={index} variant={variant} {...item} />
+    ))
+    return <Stack>{items}</Stack>
+}
+
+function ContactIcons() {
     return (
         <>
             <Head>
@@ -10,21 +153,42 @@ function Contact() {
             </Head>
             <Container
                 size="lg"
-                p="xl"
-                mt={60}
+                pt={80}
+                pb={80}
                 sx={{
-                    textAlign: "center",
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                    alignItems: "center",
+                    maxWidth: 700,
                 }}
             >
-                <Title order={1}>Contact</Title>
-                <Text size="lg">Phone: 123-456-7890</Text>
+                <Title
+                    sx={(theme) => ({
+                        fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+                        textAlign: "center",
+                        fontWeight: 900,
+                        fontSize: 38,
+                        marginBottom: theme.spacing.xl * 1.5,
+
+                        [theme.fn.smallerThan("sm")]: {
+                            fontSize: 32,
+                        },
+                    })}
+                >
+                    Contact Us
+                </Title>
+                <Box
+                    sx={(theme) => ({
+                        padding: theme.spacing.xl,
+                        borderRadius: theme.radius.md,
+                        backgroundColor:
+                            theme.colorScheme === "dark"
+                                ? theme.colors.lime[9]
+                                : theme.colors.lime[2],
+                    })}
+                >
+                    <ContactIconsList />
+                </Box>
             </Container>
         </>
     )
 }
 
-export default Contact
+export default ContactIcons
